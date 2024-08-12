4e60c85b5814281d7257076073908f2a75bba1af
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-08-12 16:26:15 +0200

    chore(config): update baseURL in vite.config.js
    - Modified baseURL in vite.config.js to reflect the new project configuration
    - Ensured correct paths and routing for the updated baseURL
    - Verified that the application builds and runs successfully with the new baseURL

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import {viteStaticCopy} from 'vite-plugin-static-copy'
 
 export default defineConfig({
-    base: '/gala-clicker/',
     plagins: [viteStaticCopy({
         targets: [
             { src: 'assets', dest: 'dist/assets' },

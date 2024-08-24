e1ee35848784e6bf00c390299e64970d341101a1
Rostyslav Lisovyi <rokokos97@gmail.com>
2024-08-24 12:39:01 +0200

    feat: integrate environment variables in main.js
    - Introduced environment variables in main.js for dynamic configuration
    - Used dotenv to securely access and manage environment-specific values
    - Enhanced flexibility by replacing hardcoded values with environment variables

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './style.css'
-    
-const SERVER_USERS_URL = 'https://lisovyi.eu/api/users/'
-const SERVER_LEVELS_URL = 'https://lisovyi.eu/api/levels/'
+
+const SERVER_USERS_URL = process.env.SERVER_USERS_URL
+const SERVER_LEVELS_URL = process.env.SERVER_LEVELS_URL
 async function updateUser(updatedUser) {
     console.log('Updated USER', updatedUser)
     console.log('Updated USER ID', updatedUser.external_id_telegram)
@@ -92,21 +92,22 @@ function initializeApp() {
     const $loading=document.querySelector("#loading-screen")
     const $navigation = document.querySelector("#navigation")
     
+    
     let availableLines = Number(localStorage.getItem('availableLines')) || 100
     let recoveryInterval = null
     let delayTimeout = null
     
-    // const LEVELS = fetchLevelsSheet();
+    const LEVELS = fetchLevels();
 
-    const LEVELS = [
-        { id: 1, name: "Student", numberOfCodeLines: 0, imgUrl: './galaStudent.webp', xlevel: 1, maxLines: 100 },
-        { id: 2, name: "Trainee", numberOfCodeLines: 3000, imgUrl: './galaTrainee.webp', xlevel: 1, maxLines: 200 },
-        { id: 3, name: "Junior", numberOfCodeLines: 100000, imgUrl: './galaJunior.webp', xlevel: 5, maxLines: 200 },
-        { id: 4, name: "Middle", numberOfCodeLines: 1000000, imgUrl: './galaMiddle.webp', xlevel: 10, maxLines: 400 },
-        { id: 5, name: "Senior", numberOfCodeLines: 10000000, imgUrl: './galaSenior.webp', xlevel: 15, maxLines: 600 },
-        { id: 6, name: "Team Lead", numberOfCodeLines: 100000000, imgUrl: './galaTeamLead.webp', xlevel: 25, maxLines: 800 },
-        { id: 7, name: "Google", numberOfCodeLines: 1000000000, imgUrl: './galaGoogle.webp', xlevel: 50, maxLines: 999 }
-    ]
+    // const LEVELS = [
+    //     { id: 1, name: "Student", numberOfCodeLines: 0, imgUrl: './galaStudent.webp', xlevel: 1, maxLines: 100 },
+    //     { id: 2, name: "Trainee", numberOfCodeLines: 3000, imgUrl: './galaTrainee.webp', xlevel: 1, maxLines: 200 },
+    //     { id: 3, name: "Junior", numberOfCodeLines: 100000, imgUrl: './galaJunior.webp', xlevel: 5, maxLines: 200 },
+    //     { id: 4, name: "Middle", numberOfCodeLines: 1000000, imgUrl: './galaMiddle.webp', xlevel: 10, maxLines: 400 },
+    //     { id: 5, name: "Senior", numberOfCodeLines: 10000000, imgUrl: './galaSenior.webp', xlevel: 15, maxLines: 600 },
+    //     { id: 6, name: "Team Lead", numberOfCodeLines: 100000000, imgUrl: './galaTeamLead.webp', xlevel: 25, maxLines: 800 },
+    //     { id: 7, name: "Google", numberOfCodeLines: 1000000000, imgUrl: './galaGoogle.webp', xlevel: 50, maxLines: 999 }
+    // ]
     function start (){
         checkAndResetDailyScore()
         checkAndResetMonthlyScore()
@@ -125,7 +126,7 @@ function initializeApp() {
             recoveryInterval = setInterval(recoverLines, 1000);
         }
         updateUserInfo();
-        fetchLevels();
+    
         fetchAllUsers().then(users => displayLeaderboard(users));
 
         $loading.style.display = 'none';
